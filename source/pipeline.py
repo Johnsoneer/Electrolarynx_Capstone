1c737fd5c1add5104d5c14fbc772854da3405064
WilliamFrederick_Johnson <iam.will.johnson@gmail.com>
2023-02-23 11:59:15 -0500

    willj_2: getting the pipeline script running and pulling together a sample_prediction notebook for us to run open-source models on.

--- source/pipeline.py
+++ source/pipeline.py
@@ -1,36 +1,51 @@
+import pandas as pd
+import numpy as np
+import os
+import wave, math, contextlib
+import speech_recognition as sr
+from moviepy.editor import AudioFileClip
+import matplotlib.pyplot as plt
 
-# import statements
-
-
+# change working directory to capstone home
 
 class pipeline():
     """
     Object for importing and cleaning electrolarynx video feeds prior to prediction
     """
-    def __init__(self):
-        #TODO: Implement pipeline base object
-        return None
-
-    def import_videos(self, source_url):
-        """
-        Method for importing new videos given a source_url or similar.
-
-        :return:
-            - imported videos (object? tbd)
-        """
-        #TODO: implement pipeline.import_videos()
+    def __init__(self, data_directory = 'data/'):
+        self.data_directory = data_directory
+        self.metadata = pd.read_csv(f"{data_directory}metadata.csv")
         
-        return None
 
-    def get_audio(self):
+    def get_audio(self, video_path):
         """
-        Method for pulling audio from imported video viles
+        Method for importing a new video given a pat
 
         :return:
-            - matrix of audio files
+            - sound_array(arr): array of sound
+            - uadioclip(obj): AudioFileClip() object
         """
-        #TODO: implement pipeline.get_audio()
-        return None
+        audio_filename = video_path.split('/')[-1].replace('.mp4','.wav')
+        audioclip = AudioFileClip(self.data_directory+video_path)
+        sound_array = audioclip.to_soundarray()
+
+        return sound_array, audioclip
+
+    def print_audio(self, video_path, sample_rate = 41000):
+        """
+        Method for displaying the waveform of a given audio file
+        """
+        audio, _obj = self.get_audio(video_path)
+        #defining a function to normalize the audio
+        def normalize_audio(audio):
+            audio = audio / np.max(np.abs(audio))
+            return audio
+        
+        #applying the function to the audio
+        audio = normalize_audio(audio)
+        plt.figure(figsize=(15,4))
+        plt.plot(np.linspace(0, len(audio) / sample_rate, num=len(audio)), audio)
+        plt.grid(True)
 
     def feature_engineering(self):
         """
@@ -42,6 +57,3 @@ class pipeline():
         #TODO: implement pipeline.feature_engineering()
         return None
 
-if __name__=="__main__":
-    pipe = pipeline()
-    pipe.import_videos("url")
